6853665bd0e0fe0fdfe5c92ffc59c6d58a69bb1e
pco <51373778+patriciacondori25@users.noreply.github.com>
2022-11-08 17:57:07 -0500

    Proyecto_Final

---
+++ funciones.js
@@ -0,0 +1,6 @@
+function datos() {
+    document.getElementById('nombre').innerHTML = 'Rut Patricia';
+    document.getElementById('apellidos').innerHTML = 'Condori Obregon';
+    document.getElementById('movil').innerHTML = '927 121 915';
+    document.getElementById('email').innerHTML = 'ccondori.patricia@gmail.com';
+}
